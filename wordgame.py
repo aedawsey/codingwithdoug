3434e86aa94a3502f63a82a67c4a4e395b559e44
aeadawsey <aedawsey@gmail.com>
2016-09-25 14:45:25 -0600

    Implemented random word generator

---
+++ wordgame.py
@@ -0,0 +1,29 @@
+import random
+
+def get_random_word():
+    words = ["pizza","cheese","apples"]
+    random_word = words[random.randint(0, len(words)- 1)]
+    return random_word
+
+def play_word_games():
+    strikes = 0
+    max_strikes = 3
+    playing = True
+
+    word=get_random_word()
+
+    while playing:
+        strikes += 1
+
+        if strikes >= max_strikes:
+            playing = False
+        
+    if strikes >= max_strikes:
+        print("Sorry. Game over.")
+    else:
+        print("Winner!")
+    
+
+print("Start the Game")
+play_word_games()
+print("End the Game")
